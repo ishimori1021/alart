b5ce742270ba93e206674368ef40b16665450aac
石森愛海 <ami.ishimori@icloud.com>
2016-05-28 20:39:42 +0900

    first commit

--- alarm/ViewController.swift
+++ alarm/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func start(){
+        
+    }
+    
+    @IBAction func stop(){
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
